7d8587dc0550adfdb860217139a911fc91fad533
pgcrobinson <pgcrobinson@gmail.com>
2020-01-30 19:11:51 +0000

    new updates

--- MachineLearning/MachineLearning.py
+++ MachineLearning/MachineLearning.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import quandl
 import math
+import numpy as np
+from sklearn import preprocessing, svm
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 quandl.ApiConfig.api_key = "g2ZYgdP4oxssaqw5HW9v"
 
@@ -20,6 +24,15 @@ forecast_out = int(math.ceil(0.01*len(df)))
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
 
-print(df.head())
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+X = preprocessing.scale(X)
+y = np.array(df['label'])
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
 
